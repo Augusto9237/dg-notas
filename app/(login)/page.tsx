285279e09fcaf54000085a206dda4e6c01f65d0e
Augusto Sousa <augusto.souza8330@gmail.com>
2025-08-31 02:08:50 -0300

    feat: create LoginPage and LoginForm components with layout and styling

---
+++ app/(login)/page.tsx
@@ -0,0 +1,32 @@
+import { GalleryVerticalEnd } from "lucide-react"
+
+import { LoginForm } from "@/components/login-form"
+import Image from "next/image"
+
+export default function LoginPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+          <Image
+            src="/Sublogo1.svg"
+            alt="Logo"
+            width={488}
+            height={400}
+            className="h-[100px] w-[188px] object-cover"
+            />
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted relative hidden lg:block">
+        <img
+          src="/foto-1.jpeg"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  )
+}
