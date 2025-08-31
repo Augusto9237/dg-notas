0be7b2fb43a5b683825b73251e3249b6e1d7a292
Augusto Sousa <augusto.souza8330@gmail.com>
2025-08-31 16:19:28 -0300

    Feat: aprimoramento da página de login com abas para funções de usuário e atualização dos estilos de layout
    - Adicionado o componente TabsDemo para alternar entre os formulários de login Aluno(a) e Professor(a).
    - Atualizado o layout.tsx para aplicar a fonte Poppins e a classe antialiasing ao corpo.
    - Modificado o login-form.tsx para incluir o botão de login do Google e melhorar a estrutura do formulário.
    - Alterado o logotipo na página de login para um novo SVG (Logo4.svg).
    - Corrigidos os caminhos de importação no layout do professor.
    - Adicionado o componente Tabs do Radix UI para melhor gerenciamento de abas.
    - Atualizado o package.json e o package-lock.json para incluir novas dependências para abas e ícones.

--- app/(login)/page.tsx
+++ app/(login)/page.tsx
@@ -1,22 +1,23 @@
 import { GalleryVerticalEnd } from "lucide-react"
 
-import { LoginForm } from "@/components/login-form"
+import { LoginForm, TabsDemo } from "@/components/login-form"
 import Image from "next/image"
 
 export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
-      <div className="flex flex-col gap-4 p-6 md:p-10">
-          <Image
-            src="/Sublogo1.svg"
-            alt="Logo"
-            width={488}
-            height={400}
-            className="h-[100px] w-[188px] object-cover"
-            />
+      <div className="flex flex-col gap-4  bg-primary">
         <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">
-            <LoginForm />
+          <div className="w-full max-w-md flex flex-col items-center gap-4">
+            <Image
+              src="/Sublogo4.svg"
+              alt="Logo"
+              width={488}
+              height={400}
+              className="h-[80px] w-[288px] object-cover"
+            />
+            {/* <LoginForm /> */}
+            <TabsDemo/>
           </div>
         </div>
       </div>
