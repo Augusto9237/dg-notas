9d1f11cdf18ce1689a85565e502c1b0c54237265
augusto.souza8330 <augusto.souza8330@gmail.com>
2025-09-18 23:11:02 +0000

    feat: atualizei o componente header

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -6,15 +6,39 @@ import { Button } from "./button";
 import { LogOut } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { ListarAvaliacoesAlunoId } from "@/actions/avaliacao";
+import { listarMentoriasAluno } from "@/actions/mentoria";
 
 export default function Header() {
   const { data: session } = authClient.useSession();
   const router = useRouter();
   const [isClient, setIsClient] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [mediaGeral, setMediaGeral] = useState(0)
+  const [totalRedacoes, setTotalRedacoes] = useState(0)
+  const [totalMentorias, setTotalMentorias] = useState(0)
 
   // Garantir que o componente só renderize no cliente
   useEffect(() => {
     setIsClient(true);
+  }, []);
+
+  useEffect(() => {
+    const fetchAvaliacoes = async () => {
+      if (session?.user.id) {
+        setIsLoading(false);
+        const avaliacoes = await ListarAvaliacoesAlunoId(session?.user.id)
+        const somaNotas = avaliacoes.reduce((acc, avaliacao) => acc + avaliacao.notaFinal, 0);
+        const media = avaliacoes.length > 0 ? somaNotas / avaliacoes.length : 0;
+        const mentorias = await listarMentoriasAluno(session?.user.id)
+
+        setMediaGeral(media);
+        setTotalRedacoes(avaliacoes.length);
+        setTotalMentorias(mentorias.length)
+      }
+    }
+    fetchAvaliacoes();
+
   }, []);
 
   async function sair() {
@@ -53,8 +77,8 @@ export default function Header() {
     <div className="bg-primary text-card px-5 py-4">
       <div className="flex items-center gap-3 mb-4">
         <Avatar className="h-12 w-12 border-2 border-secondary">
-          <AvatarImage 
-            src={session?.user?.image || "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"} 
+          <AvatarImage
+            src={session?.user?.image || "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"}
           />
           <AvatarFallback className="bg-background text-primary font-medium">
             DG
@@ -76,21 +100,21 @@ export default function Header() {
       <div className="grid grid-cols-3 gap-4">
         <Card className="text-center bg-card/10 rounded-lg backdrop-blur-sm border-none gap-0 p-2">
           <CardTitle className="text-lg font-bold text-secondary">
-            926
+            {mediaGeral}
           </CardTitle>
           <CardDescription className="text-xs opacity-90 text-card">Média Geral</CardDescription>
         </Card>
 
         <Card className="text-center bg-card/10 rounded-lg backdrop-blur-sm border-none gap-0 p-2">
           <CardTitle className="text-lg font-bold text-secondary">
-            12
+            {totalRedacoes}
           </CardTitle>
           <CardDescription className="text-xs opacity-90 text-card">Redações</CardDescription>
         </Card>
 
         <Card className="text-center bg-card/10 rounded-lg backdrop-blur-sm border-none gap-0 p-2">
           <CardTitle className="text-lg font-bold text-secondary">
-            10
+            {men}
           </CardTitle>
           <CardDescription className="text-xs opacity-90 text-card">Mentorias</CardDescription>
         </Card>
