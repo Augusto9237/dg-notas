98581638743cba4bbcc8eae133c349ec7ff05648
Augusto Sousa <augusto.souza8330@gmail.com>
2025-09-20 20:55:17 -0300

    feat: adicionei um novo componente de loading com animação e centralização, além de incluir novos ícones SVG no projeto

--- app/professor/aluno/[id]/loading.tsx
+++ app/professor/aluno/[id]/loading.tsx
@@ -1,7 +1,10 @@
+import Image from "next/image";
+
 export default function Loading() {
 
     return (
-        <div className="w-full">
-       <h1>Carregando...</h1>
-        </div>)
+        <div className="w-full flex-1 h-screen flex justify-center items-center">
+            <Image src="/Símbolo1.svg" alt="logo" height={100} width={100} className="size-28 animate-pulse" />
+        </div>
+    )
 }
