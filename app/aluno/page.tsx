62729e35fb20c272688d2b8b063e6b5ddacf285d
Augusto Sousa <augusto.souza8330@gmail.com>
2025-09-19 22:44:42 -0300

    refactor: ajustei a lógica de renderização na página do aluno para garantir que as avaliações sejam exibidas corretamente apenas quando o usuário estiver autenticado

--- app/aluno/page.tsx
+++ app/aluno/page.tsx
@@ -9,27 +9,30 @@ export default async function Page() {
     headers: await headers() // you need to pass the headers object.
   })
 
-  const avaliacoes = await ListarAvaliacoesAlunoId(session?.user.id!)
-  const criterios = await ListarCriterios()
-  
-  return (
-    <div className="w-full">
-      <main className="flex flex-col gap-4 p-5 pb-20">
-        <div className="flex items-center justify-between">
-          <h2 className="text-primary font-semibold">Suas Redações</h2>
-        </div>
-        {avaliacoes.length === 0 ? (
-          <div className="text-center text-muted-foreground py-8">
-            Nenhuma avaliação encontrada
+  if (session?.user) {
+
+    const avaliacoes = await ListarAvaliacoesAlunoId(session.user.id)
+    const criterios = await ListarCriterios()
+
+    return (
+      <div className="w-full">
+        <main className="flex flex-col gap-4 p-5 pb-20">
+          <div className="flex items-center justify-between">
+            <h2 className="text-primary font-semibold">Suas Redações</h2>
           </div>
-        ) : (
-          avaliacoes
-            .filter(avaliacao => avaliacao.tema && avaliacao.tema.nome) // Filtro adicional de segurança
-            .map((avaliacao) => (
-              <CardAvaliacao key={avaliacao.id} avaliacao={avaliacao} criterios={criterios} />
-            ))
-        )}
-      </main>
-    </div>
-  );
+          {avaliacoes.length === 0 ? (
+            <div className="text-center text-muted-foreground py-8">
+              Nenhuma avaliação encontrada
+            </div>
+          ) : (
+            avaliacoes
+              .filter(avaliacao => avaliacao.tema && avaliacao.tema.nome) // Filtro adicional de segurança
+              .map((avaliacao) => (
+                <CardAvaliacao key={avaliacao.id} avaliacao={avaliacao} criterios={criterios} />
+              ))
+          )}
+        </main>
+      </div>
+    );
+  }
 }
