619a22a3845a196f51275078b2c3642207260d80
Augusto Sousa <augusto.souza8330@gmail.com>
2025-09-20 21:14:33 -0300

    feat: melhorei o componente de loading adicionando uma animação de carregamento e centralizando o logo com um novo layout

--- app/professor/aluno/[id]/loading.tsx
+++ app/professor/aluno/[id]/loading.tsx
@@ -4,7 +4,10 @@ export default function Loading() {
 
     return (
         <div className="w-full flex-1 h-screen flex justify-center items-center">
-            <Image src="/Símbolo1.svg" alt="logo" height={100} width={100} className="size-28 animate-pulse" />
+            <div className="relative flex items-center justify-center">
+                <span className="absolute inline-flex size-28 rounded-full border-4 border-primary/80 border-t-transparent animate-spin"></span>
+                <Image src="/Símbolo1.svg" alt="logo" height={100} width={100} className="size-24 animate-pulse relative z-10" />
+            </div>
         </div>
     )
 }
