0c243ba95c6c997bd8b0607eaeac3212ddfb3c61
Augusto Sousa <augusto.souza8330@gmail.com>
2025-12-11 20:54:08 -0300

    style: atualizei o estilo da area do aluno

--- components/ui/footer-aluno.tsx
+++ components/ui/footer-aluno.tsx
@@ -30,8 +30,8 @@ export const FooterAluno = ({ className }: FooterAlunoProps) => {
     ];
 
     return (
-        <footer className={cn("fixed inset-x-0 bottom-0 px-5 pb-2", className)}>
-            <Card className="backdrop-blur-lg p-0 border-none bg-card/70">
+        <footer className={cn("fixed inset-x-0 bottom-0", className)}>
+            <Card className="backdrop-blur-lg p-0 border-none bg-card rounded-t-xl rounded-b-none shadow-[1px_1px_15px_6px_rgba(0,_0,_0,_0.1)]">
                 <nav className="flex justify-between w-full px-5 relative">
                     <Link href="/aluno" className="absolute top-0 left-1/2 transform -translate-x-1/2 object-cover">
                         <Image src="/Símbolo4.svg" alt="logo" width={100} height={100} className={cn("w-14 h-14 bg-primary rounded-full p-2 border border-2 ", pathname === "/aluno" ? "border-secondary" : "border-primary")} />
