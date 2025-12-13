a3526e3d3cfee95bfe293c24ccc5a1a251d3d2e2
Augusto Sousa <augusto.souza8330@gmail.com>
2025-12-13 15:17:18 -0300

    feat: initialize Firebase Admin SDK with service account credentials from environment variables.

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -4,21 +4,19 @@ import admin from 'firebase-admin';
 // Verifica se o app do Firebase já foi inicializado
 if (!admin.apps.length) {
   try {
-    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON as string);
-    // Inicializa o app do Firebase com as credenciais do ambiente
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-      databaseURL: process.env.FIREBASE_DATABASE_URL,
-    });
+    if (process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
+      const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+      // Inicializa o app do Firebase com as credenciais do ambiente
+      admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: process.env.FIREBASE_DATABASE_URL,
+      });
+    } else {
+      console.warn('FIREBASE_SERVICE_ACCOUNT_JSON não definido. Firebase Admin não inicializado.');
+    }
   } catch (error) {
     console.error('Erro na inicialização do Firebase Admin:', error);
   }
 }
 
-// Exporta as instâncias do Firebase Admin
-const firestoreDb = admin.firestore();
-const authAdmin = admin.auth();
-const storageAdmin = admin.storage();
-
-export { firestoreDb, authAdmin, storageAdmin };
 export default admin;
