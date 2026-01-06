c0cfed980c671fc7550e906f4a0bef5ffff46bcf
augusto.souza8330 <augusto.souza8330@gmail.com>
2026-01-06 13:23:50 +0000

    fix: A função calcularMediasPorCriterio foi refatorada para simplificar o cálculo das médias e garantir que todos os critérios sejam exibidos, mesmo que não tenham nenhuma avaliação associada.

--- components/lista-competencias-aluno.tsx
+++ components/lista-competencias-aluno.tsx
@@ -12,24 +12,27 @@ interface Props {
 export function ListaCompetenciasAluno({ criterios }: Props) {
     const { listaAvaliacoes } = useContext(ContextoAluno)
 
-    function calcularMediasPorCriterio(avaliacoes: Awaited<ReturnType<typeof ListarAvaliacoesAlunoId>>) {
-        const pontuacoesPorCriterio = avaliacoes
-            .flatMap(avaliacao => avaliacao.criterios)
-            .reduce((acc, criterio) => {
-                if (!acc[criterio.criterioId]) {
-                    acc[criterio.criterioId] = [];
-                }
-                acc[criterio.criterioId].push(criterio.pontuacao);
+    function calcularMediasPorCriterio(
+        avaliacoes: Awaited<ReturnType<typeof ListarAvaliacoesAlunoId>>,
+        criterios: Criterio[]
+    ) {
+        const scoresByCriterio = avaliacoes
+            .flatMap(a => a.criterios)
+            .reduce((acc, c) => {
+                acc[c.criterioId] = [...(acc[c.criterioId] || []), c.pontuacao];
                 return acc;
             }, {} as Record<number, number[]>);
 
-        return Object.entries(pontuacoesPorCriterio).map(([criterioId, pontuacoes]) => ({
-            criterioId: Number(criterioId),
-            media: pontuacoes.reduce((sum, current) => sum + current, 0) / pontuacoes.length,
-        }));
+        return criterios.map(criterio => {
+            const scores = scoresByCriterio[criterio.id];
+            const media = (scores && scores.length > 0)
+                ? scores.reduce((sum, score) => sum + score, 0) / scores.length
+                : 0;
+            return { criterioId: criterio.id, media };
+        });
     }
 
-    const mediasPorCriterio = calcularMediasPorCriterio(listaAvaliacoes);
+    const mediasPorCriterio = calcularMediasPorCriterio(listaAvaliacoes, criterios);
     return (
         <div className='space-y-4 h-full overflow-y-auto pb-14 scrollbar-thin scrollbar-thumb-card scrollbar-track-background'>
             {mediasPorCriterio.map((criterio, i) => (
