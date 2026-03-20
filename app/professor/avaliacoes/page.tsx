0e902f39bc431a0f8e11b54b4275c51d1e3ce437
Augusto Sousa <augusto.souza8330@gmail.com>
2026-03-20 00:47:33 -0300

    fix: corrige o erro de header

--- app/professor/avaliacoes/page.tsx
+++ app/professor/avaliacoes/page.tsx
@@ -1,9 +1,10 @@
-import { ListarAvaliacoes, ListarCriterios, listarTemas } from '@/actions/avaliacao';
+import { ListarCriterios} from '@/actions/avaliacao';
 import { FormularioTema } from '@/components/formulario-tema';
 import { TabelaTemas } from '@/components/tabela-temas';
 import { Suspense } from 'react';
 import Loading from './loading';
 import { ModalCompetencias } from '@/components/modal-competencias';
+import { HeaderTeacher } from '@/components/header-professor';
 
 export default async function Page() {
   const criterios = await ListarCriterios()
@@ -11,22 +12,16 @@ export default async function Page() {
   return (
     <Suspense fallback={<Loading />}>
       <div className="w-full h-full min-h-screen relative pt-16 overflow-y-auto">
-        <HeaderTeacher title="Aulas" description="Lista de aulas">
-          <div className='flex justify-end w-full flex-1 items-center gap-4'>
+        <HeaderTeacher title="Avaliações" description="Lista de Avaliações">
+          <div className='flex items-center gap-4'>
             <ModalCompetencias criterios={criterios} />
             <FormularioTema />
           </div>
-          <HeaderProfessor>
-            <div>
-              <h1 className=" text-xl font-bold">Avaliações</h1>
-              <p className="text-xs text-muted-foreground max-sm:leading-none">Lista de avaliações</p>
-            </div>
+        </HeaderTeacher>
 
-          </HeaderProfessor>
-
-          <main className="flex flex-col p-5 h-full">
-            <TabelaTemas />
-          </main>
+        <main className="flex flex-col p-5 h-full">
+          <TabelaTemas />
+        </main>
       </div>
     </Suspense>
   );
