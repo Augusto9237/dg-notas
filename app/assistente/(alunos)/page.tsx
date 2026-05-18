a702095eab608f2bcd239b6f09873d0da230efaf
Augusto Sousa <augusto.souza8330@gmail.com>
2026-05-18 01:21:08 -0300

    feat: implement student list page for assistant and update architectural documentation

--- app/assistente/(alunos)/page.tsx
+++ app/assistente/(alunos)/page.tsx
@@ -1,8 +1,8 @@
 
-import { TabelaAlunos } from '@/components/tabela-alunos';
 import Loading from './loading';
 import { Suspense } from 'react';
 import { HeaderTeacher } from '@/components/header-professor';
+import { TabelaAlunosAssistente } from '@/components/tabela-alunos-assistente';
 
 
 export default async function Page() {
@@ -10,9 +10,9 @@ export default async function Page() {
   return (
     <Suspense fallback={<Loading />}>
       <div className="w-full h-full min-h-screen max-h-screen max-w-screen relative pt-16 overflow-y-auto">
-        <HeaderTeacher title="Alunos" description="Lista de alunos"/>
+        <HeaderTeacher title="Alunos" description="Lista de alunos" />
         <main className="flex flex-col p-5 h-full">
-          <TabelaAlunos />
+          <TabelaAlunosAssistente />
         </main>
       </div>
     </Suspense>
